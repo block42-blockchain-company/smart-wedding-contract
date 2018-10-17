bbcf549be92bb7a9957768c29049de40d4888e28
Christian Lanz <christian.l4nz@gmail.com>
2018-10-17 17:32:11 +0200

    Add minor changes

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,8 +2,7 @@ const SmartWeddingContract = artifacts.require("./contracts/SmartWeddingContract
 
 const husbandAddress = "0x5F29482aEe907075DCD88dffAF96dAa50229b02e";
 const wifeAddress = "0x482f1A41ca69BcE106c484ec21CA726BE860Cf40";
-const writtenContractIpfsId = "QmUXxYMZQnpkDiV1ySYirx3YXpp3mekXen8nD3i9cH58RT";
 
 module.exports = function(deployer) {
-  deployer.deploy(SmartWeddingContract, husbandAddress, wifeAddress, writtenContractIpfsId);
+  deployer.deploy(SmartWeddingContract, husbandAddress, wifeAddress, "QmUXxYMZQnpkDiV1ySYirx3YXpp3mekXen8nD3i9cH58RT");
 };
