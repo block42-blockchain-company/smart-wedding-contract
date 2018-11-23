d1fcecb98631e35be1edd5a88227bc59534653f0
Christian Lanz <christian.l4nz@gmail.com>
2018-11-23 16:31:52 +0100

    Change deployment files

--- truffle.js
+++ truffle.js
@@ -1,41 +1,36 @@
-/*
- * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a
- * function when declaring them. Failure to do so will cause commands to hang. ex:
- * ```
- * mainnet: {
- *     provider: function() {
- *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>')
- *     },
- *     network_id: '1',
- *     gas: 4500000,
- *     gasPrice: 10000000000,
- *   },
- */
+const HDWalletProvider = require("truffle-hdwallet-provider-privkey");
 
-const HDWalletProviderMnemonic = require("truffle-hdwallet-provider");
-const HDWalletProviderPrivateKey = require("truffle-hdwallet-provider-privkey");
+const infuraApiKey = require("./secrets.js").infuraApiKey;
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration> to customize your Truffle configuration!
 	networks: {
 		development: {
 			provider: function() {
-				const mnemonic = require("./secrets.js").mnemonic;
-				return new HDWalletProviderMnemonic(mnemonic, "http://127.0.0.1:8545")
+				const privateKeys = require("./secrets.js").privateKeysPrivateTestnet;
+				return new HDWalletProvider(privateKeys, "http://127.0.0.1:8545")
 			},
 			network_id: "*",
 			gas: 5000000,
-			gasPrice: 50000000000
+			gasPrice: 10000000000
 		},
 		ropsten: {
 			provider: function() {
-				const privateKeys = require("./secrets.js").privateKeys;
-				const infuraApiKey = require("./secrets.js").infuraApiKey;
-				return new HDWalletProviderPrivateKey(privateKeys, "https://ropsten.infura.io/v3/" + infuraApiKey)
+				const privateKeys = require("./secrets.js").privateKeysRopstenTestnet;
+				return new HDWalletProvider(privateKeys, "https://ropsten.infura.io/v3/" + infuraApiKey)
 			},
 			network_id: "3",
 			gas: 5000000,
 			gasPrice: 50000000000
+		},
+		live: {
+			provider: function() {
+				const privateKeys = require("./secrets.js").privateKeysMainnet;
+				return new HDWalletProvider(privateKeys, "https://mainnet.infura.io/v3/" + infuraApiKey)
+			},
+			network_id: "1",
+			gas: 5000000,
+			gasPrice: 10000000000
 		}
 	}
 };
