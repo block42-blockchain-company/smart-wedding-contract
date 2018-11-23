20aa6aceb5a4978175c5aeb4d3348a83fbd815fb
Christian Lanz <christian.l4nz@gmail.com>
2018-11-23 14:33:09 +0100

    Edit wallet addresses

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 App = {
-	husbandAddress: "0x48ff344581827d855a27dd4ff0742dfc88b0de7a",
-	wifeAddress: "0x2cce6e686960945f8eb6a392f7682e0d7e814d60",
+	husbandAddress: "0x2baf3e0a838a54bc464c8e27195b891d02cac708",
+	wifeAddress: "0x0ed52194e69fb8cc504ba036cf7c3b72d66a3eb2",
 
   web3Provider: undefined,
   contracts: {},
